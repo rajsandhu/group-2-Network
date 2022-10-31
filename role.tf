dea6eb1308d4efa7c89b12b2474363b2b4ec6089
paolocalvino@gmail.com <paolo.calvino@cloudreach.com>
2022-10-31 15:14:46 +0100

    Updated the key in backend

--- role.tf
+++ role.tf
@@ -22,11 +22,7 @@ data "aws_iam_policy_document" "github_assume_role_policy" {
         condition {
             test = "StringLike"
             variable = "token.actions.githubusercontent.com:sub"
-            values = ["repo:rajsandhu/group-2-Backend:*", 
-                      "repo:rajsandhu/group-2-App-Infra:*", 
-                      "repo:rajsandhu/group-2-Kibana-AMI:*", 
-                      "repo:rajsandhu/group-2-Network:*", 
-                      "repo:rajsandhu/group-2-App-Config:*"]
+            values = ["repo:rajsandhu/group-2-Network:*"]
         }
     }
 }
